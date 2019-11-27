453e60ac90e2f4b50aea6b19f00006b7c27691aa
myothernameisreal <robertrdrane@gmail.com>
2019-11-26 18:04:50 -0600

    second commit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 <p> Creating a powershell module using info from : </p>
 <p><a href="https://powershellexplained.com/2018-08-04-Powershell-Standard-Library-Binary-Module/"> This tutorial </a></p>
 <p> Along with information from :
-<p><a href="https://support.microsoft.com/en-us/help/816149/how-to-read-from-and-write-to-a-text-file-by-using-visual-c</a></p>
+<p><a href="https://support.microsoft.com/en-us/help/816149/how-to-read-from-and-write-to-a-text-file-by-using-visual-c> This Tutorial </a></p>
---
+++ src/IOTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+
+namespace IOTest
+{
+    [Cmdlet( VerbsDiagnostic.Resolve , "IOTest")]
+    public class ResolveIOTestCommand : PSCmdlet
+    {
+        [Parameter(Position=0)]
+        [ValidateNotNullOrEmpty]
+        public string InputFileName { get; set; }
+
+        [Parameter(Position=1)]
+        [ValidateNotNullOrEmpty]
+        public string OutputFileName { get; set; }
+
+        protected override void EndProcessing()
+        {
+        	try
+        	{
+        		using (StreamReader sr = new StreamReader(InputFileName))
+        		{
+        			try
+        			{
+	        			using (StreamWriter sw = new StreamWriter(OutputFileName))
+	        			{
+		        			string line;
+		        			while ((line = sr.ReadLine()) != null)
+		        			{
+		        				sw.WriteLine(line);
+		        			}
+		        		}
+		        	}
+		        	catch (Exception e)
+		        	{
+		        		Console.WriteLine("Couldn't Write File");
+		        		Console.WriteLine(e.Message);
+		        	}
+		        }
+        	}
+        	catch (Exception e)
+        	{
+        		Console.WriteLine("Couldn't Read File");
+        		Console.WriteLine(e.Message);
+        	}
+
+            this.WriteObject(this.InputFileName);
+            this.WriteObject(this.OutputFileName);
+            base.EndProcessing();
+        }
+    }
+}
